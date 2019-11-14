6f0c616db334a1925ac37056102f7dc073868912
Ritomar Torquato <ritomar@hotmail.com>
2019-11-14 00:39:54 -0300

    Início

---
+++ 03-01.py
@@ -0,0 +1,14 @@
+# 01. Indique o resultado das expressões a seguir
+
+print('a) ((2 + 3) - (5 * 8) / 4) = ', ((2 + 3) - (5 * 8) / 4))
+print('b) ((7 ** 2) * (4 / 2)) = ', ((7 ** 2) * (4 / 2)))
+print('c) 25 % 5 = ', 25 % 5)
+print('d) 81 ** 0.5 = ', 81 ** 0.5)
+print('e) 10 > 11 and 11 < 12 = ', 10 > 11 and 11 < 12)
+print('f) (10 * 9) == (20 + 50 * 1 + 20) = ', (10 * 9) == (20 + 50 * 1 + 20))
+print('g) = ', )
+print('h) = ', )
+print('i) = ', )
+print('j) = ', )
+print('l) = ', )
+print('m) = ', )
